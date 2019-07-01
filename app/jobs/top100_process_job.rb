dc6199243ac881ce215eaf0cc62a342ef79ec1f0
Will Raben <wilmer.raben@gmail.com>
2019-06-30 21:47:01 -0600

    Reworked top100_process_job to NOT request the org_url if already existed

--- app/jobs/top100_process_job.rb
+++ app/jobs/top100_process_job.rb
@@ -8,39 +8,44 @@ include LongUrisHelper
   def perform(*args)
     url_array = LongUri.order(hits: :desc).take(100)
     url_array.each do |element|
-    url = i_to_protocol(element.protocol_id)+(element.org_url)
-      begin
-          puts "HITTING: " + url #for production demostration/debugging only, --remove
-          title = Nokogiri::HTML(Net::HTTP.get(URI.parse(url))).title
-          if !title || title.include?("301 Moved")
-            url = i_to_protocol(element.protocol_id)+"www."+(element.org_url)
-            puts "TRYING: " + url
-            title = Nokogiri::HTML(Net::HTTP.get(URI.parse(url))).title
-          end
-
-        rescue SocketError => err
-          puts "ERROR: #{err.message}"
-          title = "Error: Link appears to malformed"
-          next
-        rescue Net::OpenTimeout => err
-          puts "ERROR: #{err.message}"
-          title = "Error: Link appears to be down"
-          next
-        rescue Errno::ECONNREFUSED => err
-          title = "Error: Connection Refused by host"
-          next
-        rescue Exception => err
-          puts "ERROR: #{err.message}"
-          title = "ERROR: #{err.message}"
-          next
-      end
       hit = TopHit.where(short_url: element.short_url).first_or_initialize
+      unless hit.persisted?
+        url = i_to_protocol(element.protocol_id)+(element.org_url)
+          begin
+              puts "HITTING: " + url # production demostration/debugging only, --remove
+              title = Nokogiri::HTML(Net::HTTP.get(URI.parse(url))).title
+              if !title || title.include?("301 Moved")
+                url = i_to_protocol(element.protocol_id)+"www."+(element.org_url)
+                puts "TRYING: " + url
+                title = Nokogiri::HTML(Net::HTTP.get(URI.parse(url))).title
+              end
+
+            rescue SocketError => err
+              puts "ERROR: #{err.message}"
+              title = "Error: Link appears to malformed"
+              next
+            rescue Net::OpenTimeout => err
+              puts "ERROR: #{err.message}"
+              title = "Error: Link appears to be down"
+              next
+            rescue Errno::ECONNREFUSED => err
+              title = "Error: Connection Refused by host"
+              next
+            rescue Exception => err
+              puts "ERROR: #{err.message}"
+              title = "ERROR: #{err.message}"
+              next
+        end
         if !hit.title? || hit.title.include?("301 Moved")
           hit.title = title
         end
-      hit.hits = element.hits
-      hit.save!
-      title = nil
+        hit.hits = element.hits
+        hit.save!
+        title = nil
+      else
+        hit.hits = element.hits
+        hit.save!
+      end
     end
   end
 end
